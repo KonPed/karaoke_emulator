915a2e8e17e2827429fd30806da330cdbdf54b76
Konstantinos <kpaidarakis@gmail.com>
2016-10-23 19:10:35 +0000

    move karaoke class outside of com package

---
+++ Karaoke.java
@@ -0,0 +1,8 @@
+import com.karaoke.model.Song;
+import com.karaoke.model.SongBook;
+
+public class Karaoke {
+  public void main(String[] args) {
+    System.out.println("********** WELCOME TO KARAOKE GAME **********");
+  }
+}
