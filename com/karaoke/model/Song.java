7844fe92649acb95b0acc9e018fe5040aa0f2bc4
Konstantinos <kpaidarakis@gmail.com>
2016-10-23 18:29:57 +0000

    create the Song class and build the constructor

---
+++ com/karaoke/model/Song.java
@@ -0,0 +1,28 @@
+package com.karaoke.Song;
+
+public class Song {
+  private String artist;
+  private String title;
+  private String videoUrl;
+  
+  public Song(String artist, String title, String videoUrl) {
+    this.artist = artist;
+    this.title = title;
+    this.videoUrl = videoUrl;
+  }
+  
+  public String getArtist() {
+    return artist;
+  }
+  
+  public String getTitle() {
+    return title;
+  }
+  
+  public String getVideoUrl() {
+    return videoUrl;
+  }
+  
+  
+  
+}
