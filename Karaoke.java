5605f3d48b7f1c7f305c10d5808c59da4da69094
Konstantinos <kpaidarakis@gmail.com>
2016-10-23 19:31:56 +0000

    create a song and put it in the songBook

--- Karaoke.java
+++ Karaoke.java
@@ -2,7 +2,13 @@ import com.karaoke.model.Song;
 import com.karaoke.model.SongBook;
 
 public class Karaoke {
-  public void main(String[] args) {
+  public static void main(String[] args) {
     System.out.println("********** WELCOME TO KARAOKE GAME **********");
+    
+    Song song = new Song("Michael Jackson", "Beat It", "https://www.youtube.com/watch?v=T2PAkPp0_bY");
+    SongBook songBook = new SongBook();
+    System.out.printf("Adding %s %n", song);
+    songBook.addSong(song);
+    System.out.printf("There are %d songs %n", songBook.getSongCount());
   }
 }
